28ab09a9bde42698828a4452c178804d0335709c
Malekulastar Dhudhiyawala <53387381+Malekulastar@users.noreply.github.com>
2021-12-08 10:44:42 +0530

    Final submiit with 7 tests

--- kata_malekulastar/src/kata_malekulastar/String_Calculator.java
+++ kata_malekulastar/src/kata_malekulastar/String_Calculator.java
@@ -1,29 +1,61 @@
 package kata_malekulastar;
-import java.util.*;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class String_Calculator {
-	
-	public int Add(String text) {
-		 List<String> negatives = new ArrayList<String>();
-		 
-		 if (text == null || text.isEmpty()) {
-	            return 0;
-	        }
-		else if() {
-			
-		}
-		else {
-			
-		}
-		return 1;
-	}
-	public int Adds(String text) {
-		return 1;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s= new Scanner(System.in);
-		String numbers=s.next();
+    public int calculate(String text) {
+        List<String> negatives = new ArrayList<String>();
+        
+        char customDelimiter = getCustomDelimiter(text.split("\n")[0]);
+        //first case
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        
 
-	}
-
+        String[] chuncks = (customDelimiter == ',') ?text.split(",|\n") : text.split("\n|" + customDelimiter);
+        int total = 0;
+        int skipFirstLines = (customDelimiter == ',') ? 0 : 2;
+        
+        for (String item : chuncks) {
+            if (skipFirstLines > 0) {
+                --skipFirstLines;
+            } else {
+                int num = Integer.parseInt(item);
+                if (num < 0) {
+                    negatives.add(item);
+                }
+                total += num;
+            }
+        }
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException(
+                "negatives not allowed " + String.join(",", negatives));
+        }
+        return total;
+    }
+//check if numeric
+    private boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+//use delimiter
+    private char getCustomDelimiter(String line) {
+        if (line == null || line.isEmpty()) {
+            return ',';
+        }
+        if (isNumeric(line)) {
+            return ',';
+        }
+        if (line.length() == 1) {
+            return line.charAt(0);
+        }
+        return ',';
+    }
 }
--- kata_malekulastar/src/kata_malekulastar/String_Calculator_Test.java
+++ kata_malekulastar/src/kata_malekulastar/String_Calculator_Test.java
@@ -1,14 +1,53 @@
 package kata_malekulastar;
 
+
+//Name:Malekulastar Dhudhiyawala
+//E-mail:malikdudhiyawala@gmail.com
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.jupiter.api.Test;
+
 
 public class String_Calculator_Test {
-	
- @Test
- public void emptyStringShouldReturnZero() {
-    String_Calculator sut = new String_Calculator();
-    assertEquals(0, sut.Adds(""));
-}
 
+    // p1  ----------------
+    @Test
+    public void emptyStringShouldReturnZero() {
+        String_Calculator sut = new String_Calculator();
+        assertEquals(0, sut.calculate(""));
+    }
+    @Test
+    public void oneNumberShouldReturnItseft() {
+    	String_Calculator sut = new String_Calculator();
+        assertEquals(3, sut.calculate("3"));
+    }
+    @Test
+    public void twoNumbersShouldBeAdded() {
+    	String_Calculator sut = new String_Calculator();
+        assertEquals(7, sut.calculate("2,5"));
+    }
+    @Test
+    public void moreDigitsSupported() {
+    	String_Calculator sut = new String_Calculator();
+        assertEquals(77, sut.calculate("22,55"));
+    }
+    // p2  ----------------
+    @Test
+    public void allowNnumersAsInput() {
+    	String_Calculator sut = new String_Calculator();
+        assertEquals(45, sut.calculate("1,2,3,4,5,6,7,8,9"));
+    }
+    // p3 ------------------
+    @Test
+    public void supportNewLineAsSeparator() {
+    	String_Calculator sut = new String_Calculator();
+        assertEquals(6, sut.calculate("1\n2,3"));
+    }
+    // p3 ------------------
+    @Test
+    public void supportForCustomDelimiter() {
+    	String_Calculator sut = new String_Calculator();
+        assertEquals(10, sut.calculate(";\n1;2;3;4"));
+    }
+    
+   
 }
